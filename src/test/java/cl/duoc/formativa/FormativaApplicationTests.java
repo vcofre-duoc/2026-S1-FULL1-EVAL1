b2bf598ff5e61f4ae816ca420c34af96c2d4c309
unknown <Cetecom@SJAPCA03LC0L7.duoc.acad>
2026-03-30 10:56:38 -0300

    primer commit

---
+++ src/test/java/cl/duoc/formativa/FormativaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.formativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
